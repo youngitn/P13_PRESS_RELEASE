bec333309f1dd2df04bfa84d0b64f6cbe3fccf66
B0050 <B0050@yungshingroup.com>
2015-11-03 15:49:44 +0800

    增加兩個新聞稿夾檔欄位, 增加新聞稿範本下載連結

--- YSH/OA/P13_PRESS_RELEASE/AddRun.java
+++ YSH/OA/P13_PRESS_RELEASE/AddRun.java
@@ -1,84 +1,119 @@
 package YSH.OA.P13_PRESS_RELEASE;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import jcx.db.talk;
 import jcx.jform.hproc;
-
-
-import jcx.servlet.common.organizationServlet;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 
 import java.io.*;
 
-
-public class AddRun extends hproc{
+public class AddRun extends hproc {
 	public String action(String value) throws Throwable {
-		
-			synchronized (this) {
-				doAdd();
-			}		
+
+		synchronized (this) {
+			doAdd();
+		}
 		return value;
 	}
-	
+
 	/**
 	 * 新增申請表單
-	 * @throws Throwable 
+	 * 
+	 * @throws Throwable
 	 */
-	private void doAdd() throws Throwable{
-		if(!getValue("PNO").trim().isEmpty() ){
+	private void doAdd() throws Throwable {
+		if (!getValue("PNO").trim().isEmpty()) {
 			message("該表單已存在,請重起新單！");
-		}else{
-			String pno = getPNO(getToday("YYYYmmdd"),"PRESS_RELEASE");
-		
-			setValue("PNO",pno);
-			String UPLOADS = getValue("ATTACHMENT").trim();
-			
-			if (UPLOADS.trim().length() != 0) {
-				File F1 = getUploadFile("ATTACHMENT");
-				if (F1 != null) {
-					UPLOADS = " " + F1 + " ";
-				} else {
-					UPLOADS = "";
+		} else if (getValue("REASON").trim().length() == 0) {
+			message("請輸入原因！");
+		} else {
+			String pno = getPNO(getToday("YYYYmmdd"), "PRESS_RELEASE");
+
+			setValue("PNO", pno);
+			// String UPLOADS = getValue("ATTACHMENT").trim();
+			ArrayList<String> UPLOADS = new ArrayList<String>();
+			ArrayList<String> UPLOADS_rel = new ArrayList<String>();
+			UPLOADS.add("ATTACHMENT");
+			UPLOADS.add("PRESS_RELEASE_FILE");
+			UPLOADS.add("PRESS_RELEASE_FILE2");
+			String UPLOAD_SQL = "";
+			String UPLOAD_FIELD_SQL = "";
+			String c = ",";
+			for (String UPLOAD : UPLOADS) {
+				if (getValue(UPLOAD).trim().length() != 0) {
+					UPLOADS_rel.add(UPLOAD);
 				}
-			} else {
-				UPLOADS = "";
 			}
+			for (String UPLOAD : UPLOADS_rel) {
+
+				File F1 = getUploadFile(UPLOAD);
+//				if (UPLOADS.size() == UPLOADS.indexOf(UPLOAD)) {
+//					c = "";
+//				}
+				if (F1 != null) {
+					UPLOAD_FIELD_SQL += c + UPLOAD;
+					UPLOAD_SQL += c + " '" + F1 + "' " ;
+
+				}
+
+			}
+
 			talk t = getTalk();
-			String sql = "Insert into PRESS_RELEASE (PNO,CPNYID,DATE,EMPID,DEPT_NO,REASON,ATTACHMENT) VALUES ('"+
-							   getValue("PNO")+ "','"+getValue("CPNYID")+"','"+getValue("DATE")+"','"+getValue("EMPID")+"','"+getValue("DEPT_NO")+"','"+getValue("REASON")+"','"+
-							   UPLOADS+"')";
+			String sql = "Insert into PRESS_RELEASE (PNO,CPNYID,DATE,EMPID,DEPT_NO,REASON"+UPLOAD_FIELD_SQL+") VALUES ('"
+					+ getValue("PNO")
+					+ "','"
+					+ getValue("CPNYID")
+					+ "','"
+					+ getValue("DATE")
+					+ "','"
+					+ getValue("EMPID")
+					+ "','"
+					+ getValue("DEPT_NO")
+					+ "','"
+					+ getValue("REASON")
+					+ "'"
+					+ UPLOAD_SQL + ")";
 			String now = getNow();
-			String MUSER = getUser();			  
-			String sc1="insert into PRESS_RELEASE_FLOWC (PNO,F_INP_STAT,F_INP_ID,F_INP_TIME,F_INP_INFO) values ('"+pno+"','待處理','"+MUSER+"','"+now+"','待處理')";
-	          String sc2="insert into PRESS_RELEASE_FLOWC_HIS (PNO,F_INP_STAT,F_INP_ID,F_INP_TIME,F_INP_INFO) values ('"+pno+"','待處理','"+MUSER+"','"+now+"','待處理')";
-	          //String sc3="insert into E_SALARY_SIGN_FLOWC_HIS (PNO,F_INP_STAT,F_INP_ID,F_INP_TIME,F_INP_INFO) values ('"+pno+"','單位主管','"+MUSER+"','"+now+"','')";
-					
+			String MUSER = getUser();
+			String sc1 = "insert into PRESS_RELEASE_FLOWC (PNO,F_INP_STAT,F_INP_ID,F_INP_TIME,F_INP_INFO) values ('"
+					+ pno + "','待處理','" + MUSER + "','" + now + "','待處理')";
+			String sc2 = "insert into PRESS_RELEASE_FLOWC_HIS (PNO,F_INP_STAT,F_INP_ID,F_INP_TIME,F_INP_INFO) values ('"
+					+ pno + "','待處理','" + MUSER + "','" + now + "','待處理')";
+			// String
+			// sc3="insert into E_SALARY_SIGN_FLOWC_HIS (PNO,F_INP_STAT,F_INP_ID,F_INP_TIME,F_INP_INFO) values ('"+pno+"','單位主管','"+MUSER+"','"+now+"','')";
+
 			t.execFromPool(sql);
 			t.execFromPool(sc1);
 			t.execFromPool(sc2);
-			//t.execFromPool(sc3);
-		
+			// t.execFromPool(sc3);
+
 			t.close();
 			setEditable("SEND", false);
 			message("資料庫異動完成!");
 		}
 	}
-	private String getPNO(String inToday , String inTableName) throws SQLException, Exception{
+
+	private String getPNO(String inToday, String inTableName)
+			throws SQLException, Exception {
 		int idx = 1;
 		talk t = getTalk();
 		String pno = inToday;
-		String sql = "select max(pno) from " + inTableName + " where pno like '" + inToday + "%'";
-		
+		String sql = "select max(pno) from " + inTableName
+				+ " where pno like '" + inToday + "%'";
+
 		String theMaxPNO[][];
 		theMaxPNO = t.queryFromPool(sql);
-		if(theMaxPNO[0][0] != null && !theMaxPNO[0][0].trim().equals("")){
-			pno = inToday + StringUtils.leftPad( NumberUtils.toInt(theMaxPNO[0][0].substring(inToday.length())) +idx +"", 3, "0");
-		}
-		else{
-			pno = inToday + StringUtils.leftPad( idx +"" , 3 , "0" );
+		if (theMaxPNO[0][0] != null && !theMaxPNO[0][0].trim().equals("")) {
+			pno = inToday
+					+ StringUtils.leftPad(
+							NumberUtils.toInt(theMaxPNO[0][0].substring(inToday
+									.length())) + idx + "", 3, "0");
+		} else {
+			pno = inToday + StringUtils.leftPad(idx + "", 3, "0");
 		}
 		t.close();
 		System.gc();
--- YSH/OA/P13_PRESS_RELEASE/Init.java
+++ YSH/OA/P13_PRESS_RELEASE/Init.java
@@ -1,56 +1,75 @@
 package YSH.OA.P13_PRESS_RELEASE;
+
 import jcx.jform.hproc;
 import jcx.db.*;
 
-public class Init extends hproc{ 
-	public String action(String value)throws Throwable{
-		// 可自定HTML版本各欄位的預設值與按鈕的動作 
-		// 傳入值 value 
-		if (POSITION == 5){
-						
-		setVisible("SEND",false);
-		setVisible("QUERYPAGE",false);
-		talk t = getTalk();
-		String EMPID = getValue("EMPID");
-		String sql = "select HECNAME,DEPT_NO,DEP_NAME,CPNYID from USER_INFO_VIEW where EMPID = '"+EMPID.trim()+"'";
-		String[][] ret = t.queryFromPool(sql);
-		String HECNAME = ret[0][0];
-		String DEPT_NO = ret[0][1];
-		String DEP_NAME = ret[0][2];
-		String CPNYID = ret[0][3];
-		setValue("EMPID_NAME",HECNAME);
-		setValue("DEPT_NO_NAME",DEP_NAME);
-		setValue("EMPID",EMPID);
-		setValue("DEPT_NO",DEPT_NO);
-		setValue("CPNYID",CPNYID);
-		setValue("DATE",getToday("YYYYmmdd"));
+public class Init extends hproc {
+	public String action(String value) throws Throwable {
+		// 可自定HTML版本各欄位的預設值與按鈕的動作
+		// 傳入值 value
 		
-		String FF = getValue("ATTACHMENT").trim();	
-		if(FF.trim().length()==0) return "";
-		setValue("DOWLOAD","<a href=\""+getDownloadURL(FF.trim())+"\">附件下載</a>");
-			
-		}else{
-			/*
+		if (POSITION == 5) {
+
+			setVisible("SEND", false);
+			setVisible("QUERYPAGE", false);
+			setVisible("ATTACHMENT",false);
+			setVisible("PRESS_RELEASE_FILE",false);
+			setVisible("PRESS_RELEASE_FILE2",false);
 			talk t = getTalk();
-		String EMPID = getUser();
-		String sql = "select HECNAME,DEPT_NO,DEP_NAME,CPNYID from USER_INFO_VIEW where EMPID = '"+EMPID.trim()+"'";
-		String[][] ret = t.queryFromPool(sql);
-		String HECNAME = ret[0][0];
-		String DEPT_NO = ret[0][1];
-		String DEP_NAME = ret[0][2];
-		String CPNYID = ret[0][3];
-		setValue("EMPID_NAME",HECNAME);
-		setValue("DEPT_NO_NAME",DEP_NAME);
-		setValue("EMPID",EMPID);
-		setValue("DEPT_NO",DEPT_NO);
-		setValue("CPNYID",CPNYID);
-		setValue("DATE",getToday("YYYYmmdd"));
-		*/
+			String EMPID = getValue("EMPID");
+			String sql = "select HECNAME,DEPT_NO,DEP_NAME,CPNYID from USER_INFO_VIEW where EMPID = '"
+					+ EMPID.trim() + "'";
+			String[][] ret = t.queryFromPool(sql);
+			String HECNAME = ret[0][0];
+			String DEPT_NO = ret[0][1];
+			String DEP_NAME = ret[0][2];
+			String CPNYID = ret[0][3];
+			setValue("EMPID_NAME", HECNAME);
+			setValue("DEPT_NO_NAME", DEP_NAME);
+			setValue("EMPID", EMPID);
+			setValue("DEPT_NO", DEPT_NO);
+			setValue("CPNYID", CPNYID);
+			setValue("DATE", getToday("YYYYmmdd"));
+
+			String FF = getValue("ATTACHMENT").trim();
+			String RF1 = getValue("PRESS_RELEASE_FILE").trim();
+			String RF2 = getValue("PRESS_RELEASE_FILE2").trim();
+			String downloadString = "";
+			if (FF.trim().length() != 0) {
+				downloadString += "<a href=\"" + getDownloadURL(FF.trim())
+						+ "\">附件下載</a><br>";
+			}
+			if (RF1.trim().length() != 0) {
+				downloadString += "<a href=\"" + getDownloadURL(RF1.trim())
+						+ "\">新聞稿夾檔下載</a><br>";
+			}
+			if (RF2.trim().length() != 0) {
+				downloadString += "<a href=\"" + getDownloadURL(RF2.trim())
+						+ "\">新聞稿夾檔2下載</a><br>";
+			}
+			setValue("DOWLOAD", downloadString);
+
+		} else {
+			
+			
+			/*
+			 * talk t = getTalk(); String EMPID = getUser(); String sql =
+			 * "select HECNAME,DEPT_NO,DEP_NAME,CPNYID from USER_INFO_VIEW where EMPID = '"
+			 * +EMPID.trim()+"'"; String[][] ret = t.queryFromPool(sql); String
+			 * HECNAME = ret[0][0]; String DEPT_NO = ret[0][1]; String DEP_NAME
+			 * = ret[0][2]; String CPNYID = ret[0][3];
+			 * setValue("EMPID_NAME",HECNAME);
+			 * setValue("DEPT_NO_NAME",DEP_NAME); setValue("EMPID",EMPID);
+			 * setValue("DEPT_NO",DEPT_NO); setValue("CPNYID",CPNYID);
+			 * setValue("DATE",getToday("YYYYmmdd"));
+			 */
 		}
-		 return value;
+		
+		return value;
 	}
+
 	public String getInformation() {
 		return "---------------init().html_action()----------------";
-	}	
-	
+	}
+
 }
